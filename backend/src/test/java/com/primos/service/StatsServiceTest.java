da96c144d1db936422b2423d66dfebdca986dd5f
Juan J. Vazquez Gomez <Juanje1019@gmail.com>
2025-06-08 11:36:47 -0400

    feat: persist member stats

--- backend/src/test/java/com/primos/service/StatsServiceTest.java
+++ backend/src/test/java/com/primos/service/StatsServiceTest.java
@@ -10,11 +10,22 @@ class StatsServiceTest {
 
     static class TestService extends StatsService {
         int calls = 0;
+        MemberStats stored;
 
         @Override
         protected int fetchNftCount(String publicKey) {
             calls++;
             return 2;
+        }
+
+        @Override
+        protected MemberStats findStoredStats(String publicKey) {
+            return stored != null && stored.getPublicKey().equals(publicKey) ? stored : null;
+        }
+
+        @Override
+        protected void persistStats(MemberStats stats) {
+            this.stored = stats;
         }
     }
 
