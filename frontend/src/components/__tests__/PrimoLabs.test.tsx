79de572a591e20499c98e907773b932658d72752
Juan J. Vazquez Gomez <Juanje1019@gmail.com>
2025-06-10 14:04:49 -0400

    Move key components to pages directory

--- frontend/src/components/__tests__/PrimoLabs.test.tsx
+++ frontend/src/components/__tests__/PrimoLabs.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../../i18n';
-import PrimoLabs from '../PrimoLabs';
+import PrimoLabs from '../../pages/PrimoLabs';
 
 const renderLabs = (connected: boolean) =>
   render(
