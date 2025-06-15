ac0baa49712ce87c9f3fa4ee67d34cdb13891445
Juan J. Vazquez Gomez <Juanje1019@gmail.com>
2025-06-15 14:54:05 -0400

    feat: open NFTCard from activity

--- frontend/src/components/__tests__/Activity.test.tsx
+++ frontend/src/components/__tests__/Activity.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import Activity from '../Activity';
 import i18n from '../../i18n';
@@ -26,5 +26,34 @@ describe('Activity component', () => {
       </I18nextProvider>
     );
     expect(await screen.findByText(/Activity/i)).toBeTruthy();
+  });
+
+  test('opens NFTCard on image click', async () => {
+    const { fetchMagicEdenActivity } = require('../utils/magiceden');
+    const { getNFTByTokenAddress } = require('../services/helius');
+    (fetchMagicEdenActivity as jest.Mock).mockResolvedValueOnce([
+      {
+        tokenMint: 'mint1',
+        type: 'sale',
+        price: 1,
+        blockTime: 1,
+        signature: 'sig1',
+      },
+    ]);
+    (getNFTByTokenAddress as jest.Mock).mockResolvedValueOnce({
+      id: 'mint1',
+      image: 'img',
+      name: 'Primo 1',
+    });
+    render(
+      <I18nextProvider i18n={i18n}>
+        <Activity />
+      </I18nextProvider>
+    );
+    const img = await screen.findByAltText('Primo 1');
+    fireEvent.click(img);
+    await waitFor(() => {
+      expect(screen.getByText('Primo 1')).toBeTruthy();
+    });
   });
 });
