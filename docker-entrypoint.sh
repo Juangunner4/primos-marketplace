7d4e68babefbcf7a9e65a7bcce1d4f174bfff009
Juan J. Vazquez Gomez <Juanje1019@gmail.com>
2025-06-25 22:43:05 -0400

    Use nginx reverse proxy for render

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
-serve -s /app/frontend/build -l 3000 &
-exec java -jar /app/backend/quarkus-run.jar
+export QUARKUS_HTTP_PORT=${QUARKUS_HTTP_PORT:-8081}
+java -jar /app/backend/quarkus-run.jar &
+exec nginx -g 'daemon off;'
