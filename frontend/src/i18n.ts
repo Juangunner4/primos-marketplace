5a1d7bc42c9303baeafb45406911473d4a471f48
Juan J. Vazquez Gomez <Juanje1019@gmail.com>
2025-06-26 11:28:39 -0400

    Add Portuguese translations, env example, and cleanup

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from 'react-i18next';
 
 import en from './locales/en/en.json';
 import es from './locales/es/es.json';
+import pt from './locales/pt/pt.json';
 
 i18n
   .use(initReactI18next)
@@ -10,6 +11,7 @@ i18n
     resources: {
       en: { translation: en },
       es: { translation: es },
+      pt: { translation: pt },
     },
     lng: 'en',
     fallbackLng: 'en',
