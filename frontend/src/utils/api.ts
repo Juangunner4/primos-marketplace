0ec801dd2f479d8c237da3a38a4741df645c8462
Juan J. Vazquez Gomez <Juanje1019@gmail.com>
2025-09-15 00:28:54 -0400

    chore: upgrade axios and add size limits

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -4,6 +4,8 @@ import { getBackendUrl } from './env';
 const api = axios.create({
   baseURL: getBackendUrl(),
   headers: { 'Content-Type': 'application/json' },
+  maxContentLength: 10 * 1024 * 1024,
+  maxBodyLength: 10 * 1024 * 1024,
 });
 
 export default api;
